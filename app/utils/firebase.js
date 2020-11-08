60ff5f456fff1f8be1757522d382535fe2a4c298
jperez <jperez@Viasats-MacBook-Pro.local>
2020-11-09 00:28:15 +0100

    until firebase config

---
+++ app/utils/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCXWloa6-bcUappU_v2qDeAgLRt7zz0ZyM",
+    authDomain: "tenedores-c362c.firebaseapp.com",
+    databaseURL: "https://tenedores-c362c.firebaseio.com",
+    projectId: "tenedores-c362c",
+    storageBucket: "tenedores-c362c.appspot.com",
+    messagingSenderId: "724779101518",
+    appId: "1:724779101518:web:4a7dee1b47fb5d7f65c21f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
