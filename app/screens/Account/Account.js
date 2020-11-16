fb2ed107c64b9e6ec5c0451d81722fe75e64df64
jperez <jperez@Viasats-MacBook-Pro.local>
2020-11-16 23:09:01 +0100

    userguest screen

--- app/screens/Account/Account.js
+++ app/screens/Account/Account.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import * as firebase from "firebase";
+import Loading from "../../components/Loading";
 import UserGuest from "./UserGuest";
 import UserLogged from "./UserLogged";
 
@@ -13,7 +14,7 @@ export default function Account() {
     });
   }, []);
 
-  if (login === null) return <Text>Cargando...</Text>;
+  if (login === null) return <Loading isVisible={true} text="Cargando..." />;
 
   return login ? <UserLogged /> : <UserGuest />;
 }
