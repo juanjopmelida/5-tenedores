0bca72ae6f954496132b2fec5346d987e8b6fca1
juanjopmelida <juanjopmelida@gmail.com>
2020-12-23 00:07:53 +0100

    leción 136

---
+++ app/components/Restaurants/ListReviews.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Avatar, Rating } from "react-native-elements";
+
+import { firebaseApp } from "../../utils/firebase";
+import firebase from "firebase/app";
+
+const db = firebase.firestore(firebaseApp);
+
+export default function ListReviews(props) {
+  const { navigation, idRestaurant, setRating } = props;
+  const [userLogged, setUserLogged] = useState(false);
+
+  firebase.auth().onAuthStateChanged((user) => {
+    user ? setUserLogged(true) : setUserLogged(false);
+  });
+
+  return (
+    <View>
+      {userLogged ? (
+        <Button
+          title="Escribe una opinión"
+          buttonStyle={styles.btnAddReview}
+          titleStyle={styles.titleAddReview}
+          icon={{
+            type: "material-community",
+            name: "square-edit-outline",
+            color: "#00a680",
+          }}
+          onPress={() =>
+            navigation.navigate("add-review-restaurant", {
+              idRestaurant: idRestaurant,
+            })
+          }
+        />
+      ) : (
+        <View>
+          <Text
+            style={{ textAlign: "center", color: "#00a680", padding: 20 }}
+            onPress={() => navigation.navigate("login")}
+          >
+            Para escribir un comentario es necesario estar logeado{" "}
+            <Text style={{ fontWeight: "bold" }}>
+              Pulsa AQUÍ para inciar sesión
+            </Text>
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  btnAddReview: {
+    backgroundColor: "transparent",
+  },
+  titleAddReview: {
+    color: "#00a680",
+  },
+});
--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
-import Carousel from "../../components/Carousel";
+import { map } from "lodash";
+import { Rating, ListItem, Icon } from "react-native-elements";
 import Loading from "../../components/Loading";
+import Carousel from "../../components/Carousel";
+import Map from "../../components/Map";
+import ListReviews from "../../components/Restaurants/ListReviews";
 
 import { firebaseApp } from "../../utils/firebase";
 import firebase from "firebase/app";
@@ -15,6 +19,7 @@ export default function Restaurant(props) {
   const { navigation, route } = props;
   const { id, name } = route.params;
   const [restaurant, setRestaurant] = useState(null);
+  const [rating, setRating] = useState(0);
 
   navigation.setOptions({ title: name });
 
@@ -26,6 +31,7 @@ export default function Restaurant(props) {
         const data = response.data();
         data.id = response.id;
         setRestaurant(data);
+        setRating(data.rating);
       });
   }, []);
 
@@ -38,7 +44,86 @@ export default function Restaurant(props) {
         height={250}
         width={screenWidth}
       />
+      <TitleRestaurant
+        name={restaurant.name}
+        description={restaurant.description}
+        rating={restaurant.rating}
+      />
+      <RestaurantInfo
+        location={restaurant.location}
+        name={restaurant.name}
+        address={restaurant.address}
+      />
+      <ListReviews
+        navigation={navigation}
+        idRestaurant={restaurant.id}
+        setRating={setRating}
+      />
     </ScrollView>
+  );
+}
+
+function TitleRestaurant(props) {
+  const { name, description, rating } = props;
+  return (
+    <View style={styles.viewRestaurantTitle}>
+      <View style={{ flexDirection: "row" }}>
+        <Text style={styles.nameRestaurant}>{name}</Text>
+        <Rating
+          style={styles.rating}
+          imageSize={20}
+          readonly
+          startingValue={parseFloat(rating)}
+        />
+      </View>
+      <Text style={styles.restaurantDescription}>{description}</Text>
+    </View>
+  );
+}
+
+function RestaurantInfo(props) {
+  const { location, name, address } = props;
+
+  const listInfo = [
+    {
+      text: address,
+      iconName: "map-marker",
+      iconType: "material-community",
+      action: null,
+    },
+    {
+      text: "111 222 333",
+      iconName: "phone",
+      iconType: "material-community",
+      action: null,
+    },
+    {
+      text: "juanjopmelida@gmail.com",
+      iconName: "at",
+      iconType: "material-community",
+      action: null,
+    },
+  ];
+
+  return (
+    <View style={styles.viewRestaurantInfo}>
+      <Text style={styles.restaurantInfoTitle}>
+        Información sobre el restaurante
+      </Text>
+      <Map location={location} name={name} height={100} />
+      {map(listInfo, (item, index) => (
+        <ListItem
+          key={index}
+          title={item.text}
+          leftIcon={{
+            name: item.iconName,
+            type: item.iconType,
+            color: "#00a680",
+          }}
+          containerStyle={styles.containerListItem}
+        />
+      ))}
+    </View>
   );
 }
 
@@ -46,5 +131,33 @@ const styles = StyleSheet.create({
   viewBody: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  viewRestaurantTitle: {
+    padding: 15,
+  },
+  nameRestaurant: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  restaurantDescription: {
+    marginTop: 5,
+    color: "grey",
+  },
+  rating: {
+    position: "absolute",
+    right: 0,
+  },
+  viewRestaurantInfo: {
+    margin: 15,
+    marginTop: 25,
+  },
+  restaurantInfoTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  containerListItem: {
+    borderBottomColor: "#d8d8d8",
+    borderBottomWidth: 1,
   },
 });
